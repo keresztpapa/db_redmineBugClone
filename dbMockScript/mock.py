e0f9317e60055c5bdd7bfc6493b6521b390c0c2a
keresztpapa <keresztpapa@pop-os.localdomain>
2022-09-10 16:39:22 +0200

    user classified in python

--- dbMockScript/mock.py
+++ dbMockScript/mock.py
@@ -1,9 +1,22 @@
 from faker import Faker
-import pandas as pd
 
 faker = Faker()
-faker.name()
 
 pos = ["Angular-dev", ".NET-dev", "SQL-dev", "Fullstack-dev", "Tester"]
 
-print(faker.name(), pos[0], faker.date_time_this_month())
+
+class User:
+    def __init__(self, nev, pozi, tickets, teams):
+        self.name = nev
+        self.pos = pozi
+        self.prev_tickets = tickets
+        self.team = teams
+
+
+u = User(faker.name(), pos[0], [], "blue")
+u1 = User(faker.name(), pos[2], ["xasxda", "asd01351as"], "blue")
+
+#print(faker.name(), pos[0], faker.date_time_this_month(), faker.ssn())
+
+print(u.name, u.pos, u.prev_tickets, u.team)
+print(u1.name, u1.pos, u1.prev_tickets, u1.team)
