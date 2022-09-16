97076b09e72bff5b6e544d4d31472177afedc5e9
keresztpapa <keresztpapa@pop-os.localdomain>
2022-09-16 17:10:12 +0200

    db connection trial

---
+++ dbMockScript/mock_DB.js
@@ -0,0 +1,32 @@
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'admin',
+  database: 'bugzilla'
+});
+
+connection.connect();
+console.log("connected");
+
+var sql1 = "INSERT INTO bugs (id, name) VALUE (1,'Mario')";
+var sql2 = "INSERT INTO bugs (id, name) VALUE (2,'Peter')";
+
+connection.query(sql1, function (err, result) {
+  if (err) throw err;
+  console.log("1 record inserted");
+});
+
+connection.query(sql2, function (err, result) {
+  if (err) throw err;
+  console.log("1 record inserted");
+});
+
+connection.query("SELECT * FROM bugs", function (err, result, fields) {
+  if (err) throw err;
+  console.log(result);
+});
+
+connection.end();
