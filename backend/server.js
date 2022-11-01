8c47197a75d4254d736bf667a2404bf56471c5c9
Solyom <h880824@stud.u-szeged.hu>
2022-11-01 12:16:37 +0100

    mod

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const {resolve} = require('path');
-const index = resolve('../frontend/index.html');
+const index = resolve('../frontend/html/index.html');
+const register = resolve('../frontend/html/registerPage.html');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 var path = require('path');
 var mysql = require('mysql');
 app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../frontend/html')));
 
 app.get('/', (req, res) => res.sendFile(index));
+app.get('/index', (req, res) => res.sendFile(index));
+app.get('/register', (req, res) => res.sendFile(register));
 
 var con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'admin',
     database: 'bugzilla'
-  });
+});
 
 app.post('/add_user', urlencodedParser, function (req, res) {
     response = {
@@ -24,6 +28,11 @@ app.post('/add_user', urlencodedParser, function (req, res) {
        last_name:req.body.last_name,
        pos:req.body.position
     };
+    let sql = `INSERT INTO user (name, pos) VALUE('${req.body.first_name} ${req.body.last_name}', '${req.body.position}');`
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("record inserted");
+    });
     console.log(response);
     res.end(JSON.stringify(response));
 })
@@ -35,9 +44,7 @@ app.post('/clicked', urlencodedParser, (req, res) => {
     res.end()
     /*    
     console.log(JSON.stringify(result[0].name)); 
-    res.send(JSON.stringify(result[0].name));
-    res.end()
     */
     });
 });
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
